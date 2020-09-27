f677bebe0a21df23382e4d66c135562e4b84d171
Yegor <66310643+kutuzov13@users.noreply.github.com>
2020-09-27 22:19:13 +0300

    Create PortScanner.py

---
+++ PortScanner.py
@@ -0,0 +1,22 @@
+import socket
+
+def scan_port(ip, port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if client.connect_ex((ip, port)):
+        print(f"Port {port} close")
+    else:
+        print(f"Port {port} open")
+
+ip_domain = input('Enter plase you domain(example: google.com): ')
+if ip_domain == '':
+    print('You must specify a host!')
+
+port1 = int(input('Range port from: ')) 
+port2 = int(input('Range port before: '))
+
+try:
+    ip = socket.gethostbyname(ip_domain)
+    for i in range(port1, port2 + 1):
+        scan_port(ip, i)
+except Exception:
+    print('There was an error resolving the domain')
