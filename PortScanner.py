cc5791e92e9b61a16eb50ea286243a43e98a4d2b
Yegor <kutuzov-1313@yadnex.ru>
2021-02-09 21:45:31 +0300

    super Refactoring

--- PortScanner.py
+++ PortScanner.py
@@ -3,19 +3,6 @@ import pyfiglet
 import threading
 from queue import Queue
 
-socket.setdefaulttimeout(1)
-print_lock = threading.Lock()
-
-banner = pyfiglet.figlet_format('PORT SCANNER')
-print(banner)
-
-ip_domain = input('Enter please you host (example: google.com or 192.168.0.1): ')
-if ip_domain == '':
-    print('You must specify a host!')
-
-port1 = int(input('Range port from: '))
-port2 = int(input('Range port before: '))
-
 
 def scan_port(port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,23 +19,33 @@ def scan_port(port):
 
 def threader():
     while True:
-        worker = q.get()
-        scan_port(worker)
-        q.task_done()
+        runner = queue.get()
+        scan_port(runner)
+        queue.task_done()
 
 
-q = Queue()
+if __name__ == '__main__':
+    banner = pyfiglet.figlet_format('PORT SCANNER')
+    print(banner)
 
-for x in range(100):
-    thred = threading.Thread(target=threader, daemon=True)
-    thred.start()
+    socket.setdefaulttimeout(1)
+    print_lock = threading.Lock()
 
-try:  # translate hostname to IPv4
-    host = socket.gethostbyname(ip_domain)
-    print(f'Start scan on host {host}')
-    for worker in range(port1, port2):
-        q.put(worker)
-    q.join()
-    print(f'End scan host {host}')
-except ConnectionError:
-    print('There was an error resolving the domain')
+    ip_domain = input('Enter please you host (example: google.com or 192.168.0.1): ')
+    if ip_domain == '':
+        print('You must specify a host!')
+    port1 = int(input('Range port from: '))
+    port2 = int(input('Range port before: '))
+    queue = Queue()
+    for x in range(100):
+        thred = threading.Thread(target=threader, daemon=True)
+        thred.start()
+    try:  # translate hostname to IPv4
+        host = socket.gethostbyname(ip_domain)
+        print(f'Start scan on host {host}')
+        for worker in range(port1, port2):
+            queue.put(worker)
+        queue.join()
+        print(f'End scan host {host}')
+    except ConnectionError:
+        print('There was an error resolving the domain')
