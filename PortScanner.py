8cbff05cfc8d41bdb355329fd081af1931c1001d
Yegor <kutuzov-1313@yadnex.ru>
2021-02-09 18:56:45 +0300

    Refactoring PEP8

--- PortScanner.py
+++ PortScanner.py
@@ -1,9 +1,9 @@
 import socket
 
 
-def scan_port(ip, port):
+def scan_port(host, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if client.connect_ex((ip, port)):
+    if client.connect_ex((host, port)):
         print(f"Port {port} close")
     else:
         print(f"Port {port} open")
@@ -17,8 +17,8 @@ port1 = int(input('Range port from: '))
 port2 = int(input('Range port before: '))
 
 try:
-    ip = socket.gethostbyname(ip_domain)
+    host = socket.gethostbyname(ip_domain)
     for i in range(port1, port2 + 1):
-        scan_port(ip, i)
-except Exception:
+        scan_port(host, i)
+except ConnectionError:
     print('There was an error resolving the domain')
